62293dd52ee9f6280551f78d0e4fdf391ea5c782
FredTrotter <fred.trotter@gmail.com>
2017-12-05 22:46:18 +0000

    loop over file extensions

--- src/MustacheViewGenerator.php
+++ src/MustacheViewGenerator.php
@@ -15,7 +15,12 @@ class MustacheViewGenerator extends DURCGenerator {
 
 	public static function run_generator($class_name,$database,$table,$fields,$squash = false){
 
-		$parent_file_name = "$class_name.mustache";	
+		
+		$parent_file_names = [
+			 "$class_name.mustache",
+			 "$class_name.tpl",
+			 "$class_name.handlebar",
+			];
 
 
 		$template_text = "
@@ -34,11 +39,13 @@ class MustacheViewGenerator extends DURCGenerator {
 
 		$my_path = base_path() . '/'. MustacheViewGenerator::$suggested_output_dir;
 
-		
-		//if it does not exist.. write it... if the squash argument is possed in as true.. write it..
-		if(!file_exists($my_path.$parent_file_name) || $squash){
-                       	file_put_contents($my_path.$parent_file_name,$template_text);
-                }
+		//we did this originally becuase we did not know what file extension to use.
+		foreach($parent_file_names as $parent_file_name){
+			//if it does not exist.. write it... if the squash argument is possed in as true.. write it..
+			if(!file_exists($my_path.$parent_file_name) || $squash){
+               	        	file_put_contents($my_path.$parent_file_name,$template_text);
+                	}
+		}
 
 		return(true);
 
