2f59b65c072f7a640bf4f00f954e360ef29b812d
FredTrotter <fred.trotter@gmail.com>
2017-12-05 11:15:31 +0000

    initial

---
+++ src/DURCModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CareSet\DURC;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+/*
+	This is where we put all of the functions that we want all DURC models to inherit
+*/
+class DURCModel extends Model{
+
+
+
+
+}
