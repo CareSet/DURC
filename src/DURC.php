3eb59f95d0ec3d962794c0b2b5acffd6c95648b4
FredTrotter <fred.trotter@gmail.com>
2017-12-05 21:34:19 +0000

    better field sorting

--- src/DURC.php
+++ src/DURC.php
@@ -25,7 +25,7 @@ class DURC{
 
 	public static function getTables($db_name){
 
-	        $table_columns = DB::select('SELECT * FROM information_schema.COLUMNS where TABLE_SCHEMA = ? ORDER BY TABLE_NAME,COLUMN_NAME', [$db_name]);
+	        $table_columns = DB::select('SELECT * FROM information_schema.COLUMNS where TABLE_SCHEMA = ? ORDER BY TABLE_NAME,ORDINAL_POSITION', [$db_name]);
 	        if(is_array($table_columns) && !empty($table_columns))
 	        {
 	            	$tables = [];
