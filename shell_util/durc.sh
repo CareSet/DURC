2dbcdb2bf71ca246719cf8f4b121caf269268464
FredTrotter <fred.trotter@gmail.com>
2017-12-07 20:02:02 +0000

    two commands

--- shell_util/durc.sh
+++ shell_util/durc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-./artisan DURC --squash --DB=northwind_model --DB=northwind_data --DB=aaaDurctest
+./artisan DURC:mine --squash --DB=northwind_model --DB=northwind_data --DB=aaaDurctest
+./artisan DURC:write --squash --DB=northwind_model --DB=northwind_data --DB=aaaDurctest
 cp routes/durc.php routes/web.php
 cat routes/durc_test.php >> routes/web.php
+cp ../DURC/test_html/durc_html.mustache resources/views/DURC/
