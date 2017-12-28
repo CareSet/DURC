08a07cf8cdf347597aa00cf913c1819c7808933b
FredTrotter <fred.trotter@gmail.com>
2017-12-28 11:01:45 +0000

    save mapping centrally

--- src/DURCGenerator.php
+++ src/DURCGenerator.php
@@ -12,6 +12,45 @@ use Illuminate\Support\Facades\DB;
 class DURCGenerator{
 
 
+	public static	$column_type_map = [
+                        //integer types
+                        'int' => [ 'input_type' => 'number', ],
+                        'tinyint' => [ 'input_type' => 'number', ],
+                        'bigint' => [ 'input_type' => 'number', ],
+                        'mediumint' => [ 'input_type' => 'number', ],
+                        'smallint' => [ 'input_type' => 'number', ],
+
+                        //floats
+                        'float' => [ 'input_type' => 'number', ],
+                        'decimal' => [ 'input_type' => 'number', ],
+                        'double' => [ 'input_type' => 'number', ],
+                        'real' => [ 'input_type' => 'number', ],
+
+
+                        'date' => [ 'input_type' => 'date', ],
+                        'datetime' => [ 'input_type' => 'date', ],
+                        'timestamp' => [ 'input_type' => 'date', ],
+
+
+                        //text
+                        'char' => [ 'input_type' => 'text', ],
+                        'varchar' => [ 'input_type' => 'text', ],
+                        'tinytext' => [ 'input_type' => 'text', ],
+                        'mediumtext' => [ 'input_type' => 'text', ],
+                        'text' => [ 'input_type' => 'text', ],
+                        'longtext' => [ 'input_type' => 'text', ],
+
+                        //blob
+                        'tinyblob' => [ 'input_type' => 'file', ],
+                        'mediumblob' => [ 'input_type' => 'file', ],
+                        'blob' => [ 'input_type' => 'file', ],
+                        'longblob' => [ 'input_type' => 'file', ],
+                        'binary' => [ 'input_type' => 'file', ],
+                        'varbinary' => [ 'input_type' => 'file', ],
+
+                        ]; 
+
+
 	public static function start(){
 		//nothing here.. override me if you like..
 	}
