fff513d809119a432eaefd4193a213eba9d9bbe8
Fred Trotter <fred.trotter@gmail.com>
2019-09-03 03:54:17 -0500

    lets decorate our json

--- src/Generators/LaravelControllerGenerator.php
+++ src/Generators/LaravelControllerGenerator.php
@@ -318,7 +318,31 @@ $with_summary_array_code
     public function jsonone(Request \$request, \$$class_name"."_id){
 		\$$class_name = \App\\$class_name::find(\$$class_name"."_id);
 		\$$class_name = \$$class_name"."->fresh_with_relations(); //this is a custom function from DURCModel. you can control what gets autoloaded by modifying the DURC_selfish_with contents on your customized models
-		return response()->json(\$$class_name"."->toArray());
+		\$return_me_array = \$$class_name"."->toArray();
+		
+		//lets see if we can calculate a card-img-top for a front end bootstrap card interface
+		\$img_uri_field = \App\\$class_name::getImgField();
+		if(!is_null(\$img_uri_field)){ //then this object has an image link..
+			if(!isset(\$return_me_array['card-img-top'])){ //allow the user to use this as a field without pestering..
+				\$return_me_array['card-img-top'] = \$$class_name->\$img_uri_field;
+			}
+		}
+
+		//lets see if can calculate the same for a card title... which is actually inside a card-body.. so we will be building a little html snippet...
+		\$name_field = \App\\$class_name::getNameField();
+		if(\$name_field){ //then this object has a name
+			if(!isset(\$return_me_array['card-img-body'])){ //allow the user to use this as a field without pestering..
+				\$display_name = \$$class_name->\$name_field;
+				\$return_me_array['card-img-body'] = \"
+  <div class='card-body'>
+    <h5 class='card-title'>\$display_name</h5>
+  </div>
+\";
+
+			}
+		}
+		
+		return response()->json(\$return_me_array);
  	}
 
 
